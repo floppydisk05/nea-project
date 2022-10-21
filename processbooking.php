99c8842914f256b7da3d91224021ffd99f0a288e
Frankie B <floppydisk05@aol.com>
2022-10-21 10:48:23 +0100

    Clean up processbooking.php

--- processbooking.php
+++ processbooking.php
@@ -1,12 +1,8 @@
 <table border="1">
 <tr><th>key</th><th>value</th></tr>
 <?php 
-
-
-    foreach ($_POST as $key => $value) {
-        echo "<tr><td>".$key."</td><td>".$value."</td></tr>";
-    }
-
-
+foreach ($_POST as $key => $value) {
+    echo "<tr><td>".$key."</td><td>".$value."</td></tr>";
+}
 ?>
 </table>
