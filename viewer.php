9def445b1624a32ec6714704a6c289b1215385a6
Frankie B <floppydisk05@aol.com>
2022-11-11 12:37:09 +0000

    Update viewer

--- viewer.php
+++ viewer.php
@@ -1,4 +1,3 @@
-<a href="/">Back</a><hr>
 <?php
 // Enable error reporting
 ini_set('display_errors', '1');
@@ -17,14 +16,14 @@ if ($conn->connect_error) {
 }
 
 $bookings = $conn->query('SELECT bookings.ref, bookings.title, bookings.`start`, bookings.`end`, rooms.alias, clients.first_name AS client_fname, clients.last_name AS client_lname, users.first_name AS user_fname, users.last_name AS user_lname FROM bookings INNER JOIN rooms ON bookings.room_id = rooms.id INNER JOIN users ON bookings.created_by = users.id INNER JOIN clients ON bookings.client_id = clients.id');
+$count = implode($conn->query('SELECT COUNT(0) FROM bookings')->fetch_assoc());
+echo '<h2>Bookings <small>('.$count.' total)</small></h2><hr>';
+
 if ($bookings !== false) {
     while ($booking = $bookings->fetch_assoc()) {
-        echo '<strong>Ref:</strong> '.$booking['ref'].'<br>';
-        echo '<strong>Title:</strong> '.$booking['title'].'<br>';
-        echo '<strong>Start:</strong> '.$booking['start'].'<br>';
-        echo '<strong>End:</strong> '.$booking['end'].'<br>';
-        echo '<strong>Client:</strong> '.$booking['client_fname'].' '.$booking['client_lname'].'<br>';
-        echo '<strong>Created By:</strong> '.$booking['user_fname'].' '.$booking['user_lname'].'<br><br>';
+        echo '<strong>'.$booking['client_fname'].' '.$booking['client_lname'].'</strong><br>';
+        echo '<small>'.$booking['title'].'</small>';
+        echo '<hr>';
     }
 }
 
