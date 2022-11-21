c0b0e892a0c82a3ee66e53ca1aba73d5dde35af1
Frankie B <floppydisk05@aol.com>
2022-11-21 14:34:54 +0000

    Index begone

--- index.php
+++
@@ -1,8 +0,0 @@
-<h1>NEA Project Index</h1>
-<hr>
-<ul>
-    <li><a href="login.php">Login</a></li>
-    <li><a href="enterbooking.php">Enter Booking</a></li>
-    <li><a href="addclient.php">Add Client</a></li>
-    <li><a href="viewer.php">Viewer</a></li>
-</ul>
