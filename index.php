2bddf1a30df847593462765a75665dbfa32b6393
Frankie B <floppydisk05@aol.com>
2022-12-11 23:07:27 +0000

    Add index page

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php require_once('inc/verifylogin.inc.php'); ?>
+<ul>
+    <li><a href="enterbooking.php">Enter a booking</a></li>
+    <li><a href="addclient.php">Add a client</a></li>
+    <li><a href="viewer.php">View and search bookings</a></li>
+</ul>
